a29335891bb9ca4f0f79365c8c6238179678885f
Geek <alfredybruno@gmail.com>
2020-03-05 05:12:44 +0300

    dealing with relational operators

---
+++ relationalOperators.dart
@@ -0,0 +1,10 @@
+main(List<String> args) {
+  // equity or relational Operators ;
+  // such operators like (==, !=, <, >, <=, >=)
+
+  String name_lower = "bruno" ;
+  String name_upper = "BRUN0" ;
+
+  print (name_lower == name_upper) ;
+    
+}
