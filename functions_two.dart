c6bd5771e20afaddca60e49b9c9de8c3ea0ff782
Geek <alfredybruno@gmail.com>
2020-03-05 09:46:45 +0300

    switch construct

--- functions_two.dart
+++ functions_two.dart
@@ -7,5 +7,6 @@ print (sayHello("Bruno", "Mkonyi")) ;
 
 //  making optional argument in a function
 // It works even when age is not passed.
+// multiple arguments which are optional to a function
 
-String sayHello (String first_name, String last_name, [num age]) => "Hello $first_name $last_name" ;
+String sayHello (String first_name, [String last_name, num age]) => "Hello $first_name $last_name" ;
