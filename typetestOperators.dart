c85179de5eb8b8c49e4e9ca93307982771c84a32
Geek <alfredybruno@gmail.com>
2020-03-05 05:18:53 +0300

    dealing with type test

---
+++ typetestOperators.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+  // common type test operators include,
+  // as, is  and is!
+
+  var home_city = "dar" ;
+
+// testing the type of a variables
+
+  print (home_city is int) ;
+
+
+}
