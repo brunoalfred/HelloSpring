2daa1b181539c6f948c925d942c7782fc44864d0
Geek <alfredybruno@gmail.com>
2020-03-05 05:32:44 +0300

    dealing with logics

---
+++ logicalOperators.dart
@@ -0,0 +1,18 @@
+main(List<String> args) {
+  // logical operators include (!, &&, ||)
+
+  int number  = 100 ;
+
+// using logical operators to negate.
+
+  if ( !(number != 20) ) {
+
+    print("It is not 20") ;
+  }
+
+  else {
+    print ("It is 100") ;
+  }
+
+
+}
