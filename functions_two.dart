dd6b5754aef4313acb429307e42190edc31b9e90
Geek <alfredybruno@gmail.com>
2020-03-05 12:46:58 +0300

    null aware operator

--- functions_two.dart
+++ functions_two.dart
@@ -2,6 +2,7 @@ main(List<String> args) {
     
 
 print (sayHello("Bruno", "Mkonyi")) ;
+  sayAge("bruno", "alfred") ;
 
 }
 
@@ -10,3 +11,19 @@ print (sayHello("Bruno", "Mkonyi")) ;
 // multiple arguments which are optional to a function
 
 String sayHello (String first_name, [String last_name, num age]) => "Hello $first_name $last_name" ;
+
+// an example of fucntion without optional parameter
+
+String sayAge (String firstName, String secondName, [num age]) {
+
+  var fullname = "$firstName $secondName" ;
+
+  if (age == null) {
+
+    print ("$fullname doesn't want to provide his age") ;
+  }
+
+  else {
+    print ("The age of $fullname is $age") ;
+  }
+}
---
+++ packages.dart
@@ -0,0 +1,14 @@
+main(List<String> args) {
+
+  // null aware operator  
+int a ;
+a ??= 2 ;
+print (a) ; //prints 2
+
+
+a ??= 5 ;
+print (a) ; //print 2
+
+
+
+}
