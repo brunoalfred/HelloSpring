932f514cb7e21eeb7b8bc997aa12a251d11c6d3e
Geek <alfredybruno@gmail.com>
2020-03-05 06:00:31 +0300

    dealing with while loop icluding the idea of dead codes

--- controlFlow.dart
+++ controlFlow.dart
@@ -16,8 +16,27 @@ main(List<String> args) {
 
   // for statement
 
-  for (var i = 0 ; i < 10 ; i++  )
+ /* for (var i = 0 ; i < 10 ; i++  )
       if (i % 2 == 0)
         print ("Hello $i") ;
+
+  */
+
+
+  // while and do while statement
+
+
+  num value = 100 ;
+
+  while ( true ) {
+    if (value >= 200)  print ("hello geek") ; break ;
+
+    // this code will never get executed 
+    print ("dead code never executed") ;
+   
+  }
+
+
+
 
 }
