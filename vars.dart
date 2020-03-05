da6907f2f270166d313e5510ea8a588ac190a25e
Geek <alfredybruno@gmail.com>
2020-03-05 04:54:02 +0300

    playing around with vars

---
+++ vars.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+main (){
+// The age of James Bond is 45.
+
+String firstName = "James" ;
+String lastName = "Bond" ;
+num age = 45 ;
+
+print ("$firstName $lastName has the age of $age") ;
+
+
+}
