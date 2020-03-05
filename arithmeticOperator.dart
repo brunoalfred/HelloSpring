4734647a69db37cf4f4c148e1fe722bcd54cab31
Geek <alfredybruno@gmail.com>
2020-03-05 05:12:10 +0300

    dealing with arithmetic operators

---
+++ arithmeticOperator.dart
@@ -0,0 +1,15 @@
+main(List<String> args) {
+
+    // Resolving the are of a circles of 4 cm radius.
+    // area = 3.14 * r* r .
+
+    double r = 4 ;
+    double area ;
+
+    area = 3.14 * r * r ;
+
+    print ("The area of circle is $area") ;
+
+
+
+}
