616a93fb62cfd7b530361494950356d9db16c241
Geek <alfredybruno@gmail.com>
2020-03-05 09:04:19 +0300

    switch construct

---
+++ functions.dart
@@ -0,0 +1,55 @@
+main(List<String> args) {
+  
+  // function is called
+
+  saySomething() ;
+
+  var full_name = sayMyName() ;
+  print (full_name) ;
+
+  print ("Hello ${sayMyName()} your age is ${showage()}") ;
+
+  print ( myProgramme() ) ;
+
+}
+
+//  the function is defined with no type declaration so
+// by default the type becomes "void"
+
+// NOTE: mostly used when the function doesn't return anything
+
+saySomething () {
+
+  var name = "Bruno" ;
+  if (name.contains("B")) 
+    print ("Yeah!") ;
+
+  else {
+    print ("No!") ;
+  }
+
+}
+
+
+// For a function to return a string.
+
+String sayMyName () {
+
+  String firstName = "Bruno" ;
+  String secondName = "Alfred" ;
+  
+  return firstName + secondName ;
+
+}
+
+// return type integer
+
+int showage () {
+
+  return 34 ;
+
+}
+
+// alternatively in expressing return 
+
+String myProgramme () => "software Engineer" ; // => expr. ==  {return expr.}
