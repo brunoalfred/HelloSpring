b36e1538da33e8f920e4a051a88d4a6a6f3c72bb
Geek <alfredybruno@gmail.com>
2020-03-06 15:23:42 +0300

    using labels in the loops

--- for_loops.dart
+++ for_loops.dart
@@ -1,14 +1,24 @@
 main(List<String> args) {
   
   // for loop including the break keyword
+  // using LABELS in loops identifying
 
-  for (int i=1; i <= 10; i++) {
-    // print the value 0f i
-    print(i) ;
-    if (i==5) {
-      // break statement
-      break ;
+
+  outerLoop: for (int i=1; i <= 10; i++) {
+
+    for (int j=1; j <= 2; j++){
+
+      // print the value 0f i and j
+    print("$i $j") ;
+
+        if (i==5 && j==2) {
+          break outerLoop ;
+        }
+
+
     }
+    
+    
 
   }
 
