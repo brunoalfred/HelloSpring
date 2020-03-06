5812ee6fe9767f36ac6e69c26df3ccbd5f058951
Geek <alfredybruno@gmail.com>
2020-03-06 13:52:29 +0300

    constructors

--- class_two.dart
+++ class_two.dart
@@ -2,7 +2,16 @@ class student {
     // attribute of a student
     String name;
     String sex ;
-    
+    int age ;
+
+    //method instantiating
+    void setDetails (String name, String sex, int age) {
+      
+      name = name ;
+      sex = sex ;
+      age = age ;
+
+    }
 
 
 }
@@ -11,5 +20,11 @@ class student {
 
 
 main(List<String> args) {
+
+  //instantiation of an object from the above blueprint 
+  var pupil = new student() ;
   
+  pupil.setDetails("sonia", "female", 22) ;
+  print(pupil.sex) ;
+
 }
