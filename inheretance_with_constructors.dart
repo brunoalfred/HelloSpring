7fce78ac823998026dc01ce5afea127d6cbc8ed2
Geek <alfredybruno@gmail.com>
2020-03-08 14:21:50 +0300

    for named constractors inheritances

---
+++ inheretance_with_constructors.dart
@@ -0,0 +1,36 @@
+class Location {
+  // instance variables or member fields
+   num lat, lng ;
+
+  //  constractor implement location
+  Location (this.lat, this.lng) ;
+  
+  //  named constractors in super class
+  Location.create(this.lat, this.lng) ;
+
+}
+
+class ExtendedLocation extends Location {
+  //  member variable
+  num elevation ;
+
+
+  // inheritance of a constractor
+  // ExtendedLocation(num lat, num lng, this.elevation) : super(lat, lng) ;
+
+  // inheretance of over a named constractor
+  ExtendedLocation(num lat, num lng, this.elevation) : super.create(lat, lng) ;
+
+
+}
+
+main(List<String> args) {
+  
+  var elevated = ExtendedLocation(23.2, 34.4, 90) ;
+
+  print (elevated.elevation) ;
+
+
+
+
+}
