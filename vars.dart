11258c8dba9347ca72d7f32edc155f67abe8c06f
Geek <alfredybruno@gmail.com>
2020-03-09 08:42:43 +0300

    lists

--- vars.dart
+++ vars.dart
@@ -1,13 +1,17 @@
-import 'dart:ffi';
+class Person {
+  // member fields 
+  String name ;
+  
 
-main (){
-// The age of James Bond is 45.
-
-String firstName = "James" ;
-String lastName = "BOND" ;
-num age = 45 ;
-
-print ("$firstName $lastName has the age of $age") ;
 
 
 }
+
+main(List<String> args) {
+  
+ 
+
+  }
+
+
+
