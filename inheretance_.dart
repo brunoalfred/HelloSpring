0f83b21848a500fb746f3a4bda53e6887c486d78
Bruno Alfred <alfredybruno@gmail.com>
2020-03-11 14:14:50 +0300

    inheritance multilevel

--- inheretance_.dart
+++ inheretance_.dart
@@ -1,10 +1,62 @@
-class Animal {
-    
-     String color ;
-    void eat () ;
+ class Animal {
+     
+    String color ;
+
+    void eat () {
+      print ("Eat !") ;
+    }
+
+ }
+
+
+class Dog extends Animal {
+
+
+  String breed ;
+
+  // method
+  void bark () {
+    print ("Bark !") ;
+  }
 
 }
 
+class Cat extends Animal {
+
+
+  int age ;
+
+  void meow () {
+    print ("Meow !") ;
+  }
+
+
+}
 main(List<String> args) {
   
+  // instantiating DOG object
+  var dog = new Dog() ;
+  dog.breed = "LAbrador" ;
+  dog.color = "Black" ;
+  dog.bark() ;
+  dog.eat () ;
+
+  //  instantiating CAT object 
+
+  var cat = new Cat() ;
+  cat.color = "White" ;
+  cat.age = 3 ;
+
+
+  // for the class of animals>>>
+  /* It is allowed to call the properties of its class only since 
+      It doesn't inherite from any other class.
+  */
+
+    var animal = new Animal() ;
+    animal.color = "Yellow" ;
+    animal.eat() ;
+
+
 }
+
