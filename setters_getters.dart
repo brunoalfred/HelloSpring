ab6259d76c1446cedf035ab6b7ea911523dc2a9a
Bruno Alfred <alfredybruno@gmail.com>
2020-03-11 13:26:05 +0300

    exporing more om the concept of setters and getters

--- setters_getters.dart
+++ setters_getters.dart
@@ -7,12 +7,28 @@ void main () {
 
   // instantiating an object 
   var student = new Student() ;
+  student.name = "Peter" ;   //calling a default Setter to set the value
+  print(student.name) ;     //calling a default Getter to get the value
 
 
+  student.percentage = 423 ;
+  print (student.percentage) ;
 }
 
 class Student {
   // attributes to a student blueprint
   String name ;
+  double _percent ; //Private instance variable
+
+  // method definition
+  // custom setters defined 
+  void set percentage (double markSecured) {
+     _percent = (markSecured ~/ 500) * 100.0 ;
+  }
   
+  // custom getter defined
+  double get percentage {
+    return _percent ;
+  }
+
 }
