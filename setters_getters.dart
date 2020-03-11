a02fc9ad60c4ea7e38776f06296ab29dbb968fd0
Bruno Alfred <alfredybruno@gmail.com>
2020-03-11 13:31:09 +0300

    short hand applied

--- setters_getters.dart
+++ setters_getters.dart
@@ -8,7 +8,7 @@ void main () {
   // instantiating an object 
   var student = new Student() ;
   student.name = "Peter" ;   //calling a default Setter to set the value
-  print(student.name) ;     //calling a default Getter to get the value
+  //print(student.name) ;     //calling a default Getter to get the value
 
 
   student.percentage = 423 ;
@@ -18,17 +18,14 @@ void main () {
 class Student {
   // attributes to a student blueprint
   String name ;
-  double _percent ; //Private instance variable
+  double _percent ; //Private instance variable to its own library 
 
   // method definition
   // custom setters defined 
-  void set percentage (double markSecured) {
-     _percent = (markSecured ~/ 500) * 100.0 ;
-  }
+  void set percentage (double markSecured) => _percent = (markSecured / 500) * 100.0 ;
   
   // custom getter defined
-  double get percentage {
-    return _percent ;
-  }
+  double get percentage =>  _percent ;
+  
 
 }
