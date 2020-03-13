6594ed6ce4ed0065fa32c4448c9158af4940a7b9
Bruno Alfred <alfredybruno@gmail.com>
2020-03-13 06:39:29 +0300

    accessing both key and value in pair

--- map_and_hashMap.dart
+++ map_and_hashMap.dart
@@ -22,5 +22,15 @@ void main () {
     print(key) ;
   }
 
+  print ("\n") ;
+
   //  using for-each loop to access all values in our map
+  for (var value in fruit.values) {
+    print(value) ;
+  }
+
+  print("\n") ;
+
+  // To access both value and key in the Map.
+  fruit.forEach((key, value) => print("key: $key and value: $value")) ;
 }
