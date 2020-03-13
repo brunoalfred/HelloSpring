8a06a7ca8179ff145203f773ffcbb8d01807e49a
Bruno Alfred <alfredybruno@gmail.com>
2020-03-13 05:54:18 +0300

    abstract methods, implementation

--- abstract_classes.dart
+++ abstract_classes.dart
@@ -1,76 +1,69 @@
 // THis is an abstract class
+// In abstract methods needto be override in the sub-class.
+// An abstract class has to end with semicolon (;) instead of the method body.
+// Use abstract keyword to declare abstract class.
+
 
 abstract class Animal {
-  
-  void breathe () ;
+  void breathe();
 
-  void makeNoise () {
-    print ("Making noise !") ;
+  void makeNoise() {
+    print("Making noise !");
   }
-
 }
 
 class Comedian extends Person implements IsFunny {
-  Comedian.create(String name, String nationality) : super.create(name, nationality);
+  Comedian.create(String name, String nationality)
+      : super.create(name, nationality);
 
   @override
   void makePeopleLaugh() {
-    print ("Comedian make People laugh !") ;
+    print("Comedian make People laugh !");
   }
-
 }
 
 class TvShow implements IsFunny {
-  String name = "Bruno" ;
+  String name = "Bruno";
 
   @override
   void makePeopleLaugh() {
-
-    print ("It's Awesome")  ;
-    
+    print("It's Awesome");
   }
-
-  
-
 }
 
 abstract class IsFunny {
-    // abstract methods 
-    void makePeopleLaugh () ;
-
+  // abstract methods
+  void makePeopleLaugh();
 }
+
 class Person implements Animal {
+  // member fields
+  String name, nationality;
 
-    // member fields 
-    String name,  nationality ;
-
-    // constructor instantiation with a label
-    Person.create(this.name, this.nationality) ;
+  // constructor instantiation with a label
+  Person.create(this.name, this.nationality);
 
   @override
   void breathe() {
     // TODO: implement breathe
-    print ("Person can breath !") ;
+    print("Person can breath !");
   }
 
   @override
   void makeNoise() {
     // TODO: implement makeNoise
-    print ("Person can Make Noise !") ;
-
+    print("Person can Make Noise !");
   }
-
-    
-
 }
 
+// The abstract class, cannot be instantiated, you cannot create objects.
+
+
 main(List<String> args) {
-  
-  var bruno = new Person.create("BRUNO", "TANZANIAN") ;
+  var bruno = new Person.create("BRUNO", "TANZANIAN");
 
-  bruno.breathe() ;
+  bruno.breathe();
 
-  var mizengwe = new TvShow() ;
-  print (mizengwe.name) ;
-
+  var mizengwe = new TvShow();
+  print(mizengwe.name);
 }
