2d39a4bff1733c1e5ea6b3bcb73d051cdc3ceba6
Bruno Alfred <alfredybruno@gmail.com>
2020-03-13 06:33:37 +0300

    accessing all values in the map

--- map_and_hashMap.dart
+++ map_and_hashMap.dart
@@ -3,3 +3,24 @@
     // - Implementation of Map
     // - Based on the Hash-Table
 
+// Note ::
+  // - KEY -> has to be unique
+  // - VALUE -> can be a duplicate
+
+void main () {
+
+  Map<String, String> fruit = Map() ;
+  fruit["apple"] = "Red";
+  fruit["banana"] = "yellow" ;
+  fruit["guava"] = "green" ;
+
+  print (fruit["apple"]) ;
+
+  // using for each loop to access all keys in our map.
+
+  for (var key in fruit.keys) {
+    print(key) ;
+  }
+
+  //  using for-each loop to access all values in our map
+}
