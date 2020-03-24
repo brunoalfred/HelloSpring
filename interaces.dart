9ef334adf7abe78528b6616690f4263d709ee79d
Bruno Alfred <alfredybruno@gmail.com>
2020-03-24 20:42:33 +0300

    interfaces in dart language

---
+++ interaces.dart
@@ -0,0 +1,25 @@
+
+
+// Implementation of interfaces in dart language
+
+main() {
+	
+}
+
+
+class Button {
+
+	void print_data () {
+
+		print ("Pressed");
+	
+	}
+}
+
+class RaisedButton implements Button {
+	
+	void Print_data () {
+		print ("When I'm Pressed");
+	}
+
+}
