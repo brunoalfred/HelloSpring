d0bf1ea436c624fca37852ce780ccb66d01ae4cf
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 16:03:06 +0300

    async, await and future in dart

--- AsyncProgramming.dart
+++ AsyncProgramming.dart
@@ -1,16 +1,21 @@
 import 'dart:async';
 
-void main() {}
+void main() {
+  printFileContent();
+}
 
 //
-printFileCOntent() {}
+printFileContent() {
+  Future<String> fileContent = downLoadFile();
+  print('The content of the file is --> $fileContent');
+}
 
 // To download a FIle [Dummy LONG running Operation]
 
-downLoadFile() {
-  Future<String> result = Future.delayed(Duration(seconds: 6.0), () {
+Future<String> downLoadFile() {
+  Future<String> result = Future.delayed(Duration(seconds: 6), () {
     return 'My Secret File Content';
   });
 
-  return result ;
+  return result;
 }
