2cbb87a7f4ddcc0199f10f069ba6395fb9e0c7a8
Bruno Alfred <alfredybruno@gmail.com>
2020-04-02 15:30:13 +0300

    handling errors in

--- asynchronousProgramming/handling_error.dart
+++ asynchronousProgramming/handling_error.dart
@@ -2,4 +2,22 @@ import 'dart:async';
 
 // making a asynchronous to get USER name
 
-Future<String> fetch
+Future<String> fetchNewUsername() async {
+  return Future.delayed(Duration(seconds: 1), () => throw "jane_smith_92");
+}
+
+// Printing the new UserName
+Future<String> printUserName() async {
+// Error handling part comes now
+  try {
+    var userName = await fetchNewUsername();
+    return "The New User\'s Name Is; $userName";
+  } catch (err) {
+    print("Error Caught is: $err");
+  }
+}
+
+Future<void> main() async {
+  // Once an error is thrown no Value will be Printed
+  print(await printUserName());
+}
