175facc233373934ca27349f36386cfcd3b5e41d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-02 14:27:03 +0300

    implementing FUTURE

--- asynchronousProgramming/quiz_one.dart
+++ asynchronousProgramming/quiz_one.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
 Future<String> fetchRole() async {
+  // To fetch user Role, Its an asyncronous operations
   return Future.delayed(Duration(seconds: 5), () => "Programmer");
 }
 
-
-
-
-
-
+Future<int> fetchLoginAmount() async {
+  return Future.delayed(Duration(seconds: 5), () => 10);
+} 
