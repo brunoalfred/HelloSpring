ca214a41a2fccb6aa30e1ee609c2e7f71ef1a7f0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-02 13:56:42 +0300

    documenting the code

---
+++ await_and_async.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+/* 
+
+The ``async`` and ``await`` keyword are to provide declarative way to define 
+ asyncronous functions
+ GuideLines to using of the WORDS:
+        * To define an async function, add async before the function body:
+        * The await keyword works only in async functions.
+
+ */
+
