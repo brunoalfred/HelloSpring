c278870e294c8e18470a806742c52663a38b0abd
Bruno Alfred <alfredybruno@gmail.com>
2020-04-03 11:35:33 +0300

    serialization and deserialization docs

--- using_json/serialization.dart
+++ using_json/serialization.dart
@@ -3,7 +3,9 @@ This discussion involves issues of dart serialization and deserializations
 
     LIBRARIES AND PACKAGES
       * dart:convert
-        -converts for both JSON and UTF-8
+        -converts for both JSON and UTF-8(the character encoding required by JSON)
       * package:json_serializable
+        -An easy-to-use code generation package...
       * package:built_value
+        -A powerful, opinionated  alternative to JSON_serializable
  */
