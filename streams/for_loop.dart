3bb8cbe44b9fe12d76928de4d8eb567a0d783536
Bruno Alfred <alfredybruno@gmail.com>
2020-04-03 09:15:10 +0300

    the streams in asynchronous programming

--- streams/for_loop.dart
+++ streams/for_loop.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
-void main() {
-  Future<int> sumStream(Stream<int> stream) async {
-    var sum = 0;
-    await for (var value in stream) {
-      sum += value;
-    }
-    return sum;
+void main() {}
+Future<int> sumStream(Stream<int> stream) async {
+  var sum = 0;
+  await for (var value in stream) {
+    sum += value;
   }
+  return sum;
 }
