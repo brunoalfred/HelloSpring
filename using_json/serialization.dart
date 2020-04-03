0e371de130aa6a18c4a6ac7ec9bf4b1539ae3a9a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-03 11:32:34 +0300

    serialization and deserialization docs

--- using_json/serialization.dart
+++ using_json/serialization.dart
@@ -1,2 +1,9 @@
-/* Most mobile and Web apps use JSON for tasks such as 
-exchanging data with web servers */
+/* Most mobile and Web apps use JSON for tasks such as exchanging data with web servers &&
+This discussion involves issues of dart serialization and deserializations
+
+    LIBRARIES AND PACKAGES
+      * dart:convert
+        -converts for both JSON and UTF-8
+      * package:json_serializable
+      * package:built_value
+ */
