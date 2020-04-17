8500ac95a928909e3130dd18d387084c0ef6b0b6
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 21:05:44 +0300

    inline commenting

--- utils.dart
+++ utils.dart
@@ -1,18 +1,19 @@
 // A program that involve asyncronous operation, since it waits for a call
 // so as it can send a text message
 
-// 
+// Waiting for a call first
 Future<String> callWaiting() async {
   var call = await Future.delayed(Duration(seconds: 2), () => 'Phone Ringing');
   return call;
 }
 
-
+// Then sending a message
 Future<String> sendMessage () async {
   var info = await callWaiting();
   return Future.delayed(Duration(seconds: 2), () => 'Message Sent');
 }
 
+// Implementing the main function where the Program Excutes
 Future<void> main() async{
     String call = await callWaiting();
     print(call);
