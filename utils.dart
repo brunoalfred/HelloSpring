d0b0e03192ed8e9209df6cab9d7a498328304960
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 20:58:31 +0300

    created a function waiting for a call

--- utils.dart
+++ utils.dart
@@ -1,5 +1,8 @@
-
-// A program that involve asyncronous operation, since it waits for a call 
+// A program that involve asyncronous operation, since it waits for a call
 // so as it can send a text message
 
+Future<String> callWaiting() async {
+  var call = await Future.delayed(Duration(seconds: 2), () => 'Phone Ringing');
+  return call;
+}
 
