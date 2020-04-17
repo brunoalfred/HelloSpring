b205913e074b9ea49807e6cca6dc26a8fd16402a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 21:09:23 +0300

    commenting

--- utils.dart
+++ utils.dart
@@ -9,12 +9,15 @@ Future<String> callWaiting() async {
 
 // Then sending a message
 Future<String> sendMessage () async {
+  // awaiting for the call function first before sending the Message
   var info = await callWaiting();
+  // Then the message is being sent after we have received the call
   return Future.delayed(Duration(seconds: 2), () => 'Message Sent');
 }
 
 // Implementing the main function where the Program Excutes
 Future<void> main() async{
-    String call = await callWaiting();
-    print(call);
+  // The 
+    String status = await sendMessage();
+    print(status);
 }
