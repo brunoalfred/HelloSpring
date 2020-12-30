fb23dd26eae5b8bb282629e78a7e32ac9eee984e
bruno alfred <alfredybruno@gmail.com>
2020-12-30 20:19:59 +0300

    Implementing Data layer Interface

--- src/main/java/me/brunoalfred/demo/Dao/StudentDao.java
+++ src/main/java/me/brunoalfred/demo/Dao/StudentDao.java
@@ -3,20 +3,24 @@ package me.brunoalfred.demo.Dao;
 import me.brunoalfred.demo.Entity.Student;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public interface StudentDao {
+
+
+
+
+
     Collection<Student> getAllStudent();
 
     Student getStudentbyId(int id);
 
     void removeStudentbyId(int id);
 
-    void updateStudent(int Id);
+    void updateStudent(Student student);
 
-    void insertStudentToDb(int id);
-
-
-
+    void insertStudentToDb(Student student);
 
 
 }
